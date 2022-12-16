556c354800ed0730efa5a58d33a78e89d692ce36
magashay <115600013+magashay@users.noreply.github.com>
2022-12-15 17:02:45 -0800

    Update Iteration2.java

--- Iteration2.java
+++ Iteration2.java
@@ -1,85 +1,98 @@
-import javax.swing.*;
+/*
+ *
+ * @Author Masse
+ */
 import java.awt.*;
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
+import java.io.*;
 
 public class Iteration2 extends JFrame{
 
-    private static final Dimension FRAME_SIZE = new Dimension(550, 350);
 
-    private JFrame frame;
-
-    private JButton exportButton;
-
+    private JList<File> fileList;
+    private DefaultListModel<File> listModel;
     private JButton importButton;
+    private JButton exportButton;
+    private JButton deleteButton;
 
-    private JPanel panel;
 
-    private File savedFile;
-    
-    private File file = new File(".");
+    public Iteration2() {
+        super("Import Export");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(650, 500);
+        setLocationRelativeTo(null);
 
-    private JFileChooser fileChooser = new JFileChooser();
-    public IterationTwo(){
-        Initialize();
-    }
+        // Set up list model and file list
+        listModel = new DefaultListModel<>();
+        fileList = new JList<>(listModel);
+        fileList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
-    private void Initialize(){
-        frame = new JFrame("Export / Import");
-        frame.setVisible(true);
-        frame.setSize(FRAME_SIZE);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        panel = new JPanel();
-        exportButton = new JButton("Export");
-        exportButton.setFocusable(false);
+        // Set up buttons
         importButton = new JButton("Import");
-        importButton.setFocusable(false);
-        panel.add(exportButton);
-        panel.add(importButton);
-        frame.add(panel, BorderLayout.CENTER);
-        addExportEvent();
-        addImportEvent();
+        importButton.addActionListener(new importActionListener());
+        deleteButton = new JButton("Delete");
+        deleteButton.addActionListener(new DeleteActionListener());
+        exportButton = new JButton("Export");
+        exportButton.addActionListener(new exportActionListener());
 
+        // Set up layout
+        setLayout(new BorderLayout());
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(importButton);
+        buttonPanel.add(exportButton);
+        buttonPanel.add(deleteButton);
+        add(buttonPanel, BorderLayout.SOUTH);
+        add(new JScrollPane(fileList), BorderLayout.CENTER);
+
+       
+        setVisible(true);
     }
 
-    private void addExportEvent(){
-        exportButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("export button clicked");
-                fileChooser.setDialogTitle("File to Export");
-                int userSelection = fileChooser.showSaveDialog(frame);
-                if(userSelection == fileChooser.APPROVE_OPTION){
-                    file = fileChooser.getSelectedFile();
-                    System.out.println("File Exported to: "+file.getAbsolutePath());
+    // Action listener for uploading a file
+    private class importActionListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            int returnValue = fileChooser.showOpenDialog(Iteration2.this);
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = fileChooser.getSelectedFile();
+                listModel.addElement(selectedFile);
+            }
+        }
+    }
+
+
+
+    // Action listener for deleting a file
+    private class DeleteActionListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            int selectedIndex = fileList.getSelectedIndex();
+            if (selectedIndex != -1) {
+                listModel.remove(selectedIndex);
+            }
+        }
+    }
+    //Action listener for exporting a file
+    private class exportActionListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            int selectedIndex = fileList.getSelectedIndex();
+            if (selectedIndex != -1) {
+                File selectedFile = listModel.get(selectedIndex);
+                // Save the selected file
+                JFileChooser fileChooser = new JFileChooser();
+                int result = fileChooser.showSaveDialog(null);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File saveFile = fileChooser.getSelectedFile();
+
                 }
             }
-        });
+        }
     }
 
-    private void addImportEvent(){
-        importButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("import button clicked");
-                fileChooser.setDialogTitle("File to Import");
-                int userDestination = fileChooser.showOpenDialog(frame);
-                if(userDestination == fileChooser.APPROVE_OPTION){
-                    file = fileChooser.getSelectedFile();
-                    System.out.println("Selected file: "+file.getAbsolutePath());
-                }
-            }
-        });
-    }
 
-    public static void main(final String [] theArgs) {
-    	EventQueue.invokeLater(new Runnable() {
-    		@Override
-    		public void run() {
-    			new IterationTwo().Initialize();
-    		}
-    	});
+    public static void main(String[] args) {
+        new Iteration2();
     }
 }
