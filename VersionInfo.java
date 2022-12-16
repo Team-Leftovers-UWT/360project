c8a06ea85d4b89f89cea36eef81cf36cc74d101a
Deep <115367789+deepuh@users.noreply.github.com>
2022-12-15 22:33:44 -0800

    added javadocs to all methods and kurtis as author

--- VersionInfo.java
+++ VersionInfo.java
@@ -2,7 +2,7 @@
  * This class hold the info displayed on the about page
  * and is called by the AppGUI class.
  *
- * @author Deep Singh
+ * @author Deep Singh, Kurtis Copeland
  */
 public class VersionInfo {
 	private final static String D1 = "Deep Singh";
@@ -13,32 +13,66 @@ public class VersionInfo {
 	private final static String userName = "user";
 	private final static String myGithub = "https://github.com/Team-Leftovers-UWT/360project";
 	private final static String myVersion = "v.0.1.0";
-	
+	/**
+	 * this method returns dev1
+	 * @author Deep
+	 */
 	public static String getD1() {
 		return D1;
 	}
+	/**
+	 * this method returns dev2
+	 * @author Deep
+	 */
 	public static String getD2() {
 		return D2;
 	}
+	/**
+	 * this method returns dev3
+	 * @author Deep
+	 */
 	public static String getD3() {
 		return D3;
 	}
+	/**
+	 * this method returns dev4
+	 * @author Deep
+	 */
 	public static String getD4() {
 		return D4;
 	}
+	/**
+	 * this method returns dev5
+	 * @author Deep
+	 */
 	public static String getD5() {
 		return D5;
 	}
+	/**
+	 * this method returns our github link
+	 * @author Deep
+	 */
 	public static String getGitHub() {
 		return myGithub;
 	}
+	/**
+	 * this method returns version of this app
+	 * @author Deep
+	 */
 	public static String getVersion() {
 		return myVersion;
 	}
+	/**
+	 * this method returns the user's name
+	 * @author Deep
+	 */
 	public static String getUser() {
 		return userName;
 	}
-	
+	/**
+	 * this method returns all the devs in an array
+	 * @author Kurtis
+	 */
 	public static String[] getDevs() {
 		String[] output = new String[5];
 		output[0] = D1;
